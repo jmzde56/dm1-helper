ff59f417bdd99846e93f083d6fa5a5c171c2ad0a
jmzde56 <jmzde56@gmail.com>
2026-02-12 10:15:19 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = 'dm1-helper-v9.3';
+const ASSETS = [
+    './',
+    './index.html',
+    './style.css',
+    './motor_dm1_v9.3.js',
+    './manifest.json',
+    './icon-192.png',
+    './icon-512.png'
+];
+
+self.addEventListener('install', (event) => catchError(event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+)));
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((keys) => {
+            return Promise.all(
+                keys.filter(key => key !== CACHE_NAME)
+                    .map(key => caches.delete(key))
+            );
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
+
+function catchError(promise) {
+    return promise.catch(err => console.error("SW Error:", err));
+}
